633c616e6c1c954a7a14541e8ec68c9f59c0e29e
Yoni Davidson <yoni@ariga.io>
2021-08-11 12:37:10 +0300

    add code

--- blog/collector_example.go
+++ blog/collector_example.go
@@ -1,17 +1,21 @@
-package entprom
+package blog
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-var labelNames []string = []string{"label_1", "label_2"}
+var labelNames = []string{"label_1", "label_2"}
 
 var exampleCollector = promauto.NewCounterVec(
 	prometheus.CounterOpts{
-		Name:        "ent_operation_total",
-		Help:        "Number of ent mutation operations",
+		Name:        "example_metric_total",
+		Help:        "Number of example metrics used",
 		ConstLabels: map[string]string{"key": "value"},
 	},
-	entLabels,
+	labelNames,
 )
+
+func useCollector(){
+	exampleCollector.WithLabelValues("value1","value2").Inc()
+}
