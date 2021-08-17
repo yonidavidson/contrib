fa65a28e86fc968a87a6b87460720f703747a383
Yoni Davidson <yoni@ariga.io>
2021-08-17 10:41:05 +0300

    update extension

--- entgql/removeoldgeneratedfiles.go
+++ entgql/removeoldgeneratedfiles.go
@@ -1,17 +1,17 @@
 package entgql
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 	tparse "text/template/parse"
 
 	"entgo.io/ent/entc/gen"
+	"github.com/prometheus/common/log"
 )
 
 // RemoveOldGeneratedFiles removes old generated files without gql
-func RemoveOldGeneratedFiles(prefix string) gen.Hook {
+func RemoveOldGeneratedFiles() gen.Hook {
 	return func(next gen.Generator) gen.Generator {
 		return gen.GenerateFunc(func(g *gen.Graph) error {
 			for _, rootTemplate := range AllTemplates {
@@ -22,8 +22,10 @@ func RemoveOldGeneratedFiles(prefix string) gen.Hook {
 					if !strings.HasPrefix(template.Name(), "gql_") {
 						continue
 					}
-					fmt.Println(rootTemplate.Name(), "->", template.Name())
-					deleteByName(g, strings.TrimPrefix(template.Name(), "gql_"))
+					name := strings.TrimPrefix(template.Name(), "gql_")
+					if deleteByName(g, name) != nil {
+						log.Info("deleted ", name)
+					}
 				}
 			}
 			return next.Generate(g)
@@ -39,9 +41,10 @@ func deleteByName(g *gen.Graph, name string) error {
 	}
 	err := os.Remove(filepath.Join(g.Target, name+".go"))
 	if !os.IsNotExist(err) {
-		println("deleted:", name)
 		return err
 	}
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 	return nil
 }
-
